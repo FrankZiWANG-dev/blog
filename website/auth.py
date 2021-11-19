1fac211ca9dd93987aed9df74927e573f749fc54
FrankZiWANG-dev <wang.frank.zi@gmail.com>
2021-11-19 16:55:06 +0100

    posts by page but paging doesnt work

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,52 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
-    return "Login"
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
 
-@auth.route("/sign-up")
-def sign_up():
-    return "Sign up"
+        user = User.query.filter_by(username = username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                login_user(user, remember=True)
+                return redirect(url_for('auth.dashboard'))
+            else:
+                flash('Incorrect password', category='error')
+        else:
+            flash('No user', category='error')
 
-@auth.route("/logout")
+    return render_template("login.html")
+
+@auth.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@auth.route("/logout", methods=['GET', 'POST'])
+@login_required
 def logout():
-    return "Logout"
+    logout_user()
+    return render_template("logout.html")
+
+
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        
+        new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user, remember=True)
+        flash('Account created!', category='success')
+        return redirect(url_for('views.dashboard'))
+
+    return render_template("sign_up.html")
--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request, redirect, url_for
+from .models import Post
+from . import db
 views = Blueprint("views", __name__)
 
 @views.route("/")
@@ -11,9 +12,34 @@ def home():
 def about_me():
     return render_template("about-me.html")
 
-@views.route("/posts")
-def posts():
-    return render_template("posts.html")
+@views.route("/posts/<page>")
+
+
+def posts(page):
+    posts = Post.query.all()
+    posts.reverse()
+    grouped = []
+    for x in range(0, len(posts), 3):
+        chunk = [posts[x:x+3]]
+        grouped.insert(1, chunk)
+    page = 1
+    posts_by_page = grouped[page-1]
+    return render_template("posts.html", posts_by_page = posts_by_page)
+
+
+
+@views.route("/new-post", methods=["GET", "POST"])
+def new_post():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        
+        new_post = Post(title=title, content=content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('auth.dashboard'))
+
+    return render_template("new-post.html")
 
 @views.route("/documentation")
 def documentation():
