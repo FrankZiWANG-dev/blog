d49cf4aabd9f0c4cfddf4465c46f69a6adce825b
FrankZiWANG-dev <wang.frank.zi@gmail.com>
2021-11-22 21:19:19 +0100

    need to fix links

--- website/models.py
+++ website/models.py
@@ -12,3 +12,11 @@ class Post(db.Model):
     title = db.Column(db.String(150))
     content = db.Column(db.String(20000))
     date = db.Column(db.DateTime, default=func.now())
+    image = db.Column(db.Text(), unique=True, nullable=False)
+
+class Link(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150))
+    text = db.Column(db.String(20000))
+    date = db.Column(db.DateTime, default=func.now())
+    link = db.Column(db.String(1000))
--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from .models import Post
+from .models import Post, Link
 from . import db
 views = Blueprint("views", __name__)
 
@@ -12,38 +12,87 @@ def home():
 def about_me():
     return render_template("about-me.html")
 
-@views.route("/posts/<page>")
+@views.route("/posts")
 
 
-def posts(page):
+def posts():
     posts = Post.query.all()
     posts.reverse()
     grouped = []
     for x in range(0, len(posts), 3):
         chunk = [posts[x:x+3]]
         grouped.insert(1, chunk)
-    page = 1
+
+    page = request.args.get('page', default = 1, type = int)
     posts_by_page = grouped[page-1]
-    return render_template("posts.html", posts_by_page = posts_by_page)
 
+    previous = page-1
+    if previous == 0:
+        previous_page = 'posts?page='+str(page)
+    else:
+        previous_page = "posts?page="+str(previous)
 
+    next = page+1
+    if next > len(grouped):
+        next_page = 'posts?page='+str(page)
+    else:
+        next_page = "posts?page="+str(next)
+
+    return render_template("posts.html", posts_by_page = posts_by_page, page = page, previous_page = previous_page, next_page = next_page)
 
 @views.route("/new-post", methods=["GET", "POST"])
 def new_post():
     if request.method == 'POST':
         title = request.form.get('title')
         content = request.form.get('content')
+        image = request.form.get('image')
         
-        new_post = Post(title=title, content=content)
+        new_post = Post(title=title, content=content, image=image)
         db.session.add(new_post)
         db.session.commit()
         return redirect(url_for('auth.dashboard'))
 
     return render_template("new-post.html")
 
-@views.route("/documentation")
+@views.route("/resources")
 def documentation():
-    return render_template("documentation.html")
+    links = Link.query.all()
+    links.reverse()
+    grouped_links = []
+    for x in range(0, len(posts), 3):
+        chunk_links = [posts[x:x+3]]
+        grouped_links.insert(1, chunk_links)
+
+    page_links = request.args.get('page', default = 1, type = int)
+    links_by_page = grouped_links[page_links-1]
+
+    previous_links = page_links-1
+    if previous_links == 0:
+        previous_page_links = 'links?page='+str(page_links)
+    else:
+        previous_page_links = "links?page="+str(previous_links)
+
+    next_links = page_links+1
+    if next_links > len(grouped_links):
+        next_page_links = 'links?page='+str(page_links)
+    else:
+        next_page_links = "links?page="+str(next)
+
+    return render_template("documentation.html", page_links = page_links, links_by_page = links_by_page, previous_page_links = previous_page_links, next_page_links = next_page_links)
+
+@views.route("/new-link", methods=["GET", "POST"])
+def new_link():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        text = request.form.get('text')
+        link = request.form.get('link')
+        
+        new_link = Link(title=title, text=text, link=link)
+        db.session.add(new_link)
+        db.session.commit()
+        return redirect(url_for('auth.dashboard'))
+
+    return render_template("new-link.html")
 
 @views.route("/contact")
 def contact():
